8da90857d3fa2a89fc742636839af51b979d9c6b
OZ <paelsoft2@g.com>
2017-07-29 08:29:07 +0700

    first commit

---
+++ QnABot/Dialogs/RootDialog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using QnAMakerDialog;
+
+namespace QnABot.Dialogs
+{
+    [Serializable]
+    [QnAMakerService("cc4c35274f5b45f2a3546d18e7fa3bbf", "93dcf6d6-9273-4ccb-a431-512153ae85f6")]
+
+    public class RootDialog : QnAMakerDialog<object>
+    {
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            await context.PostAsync($"Sorry, I couldn't find an answer for '{originalQueryText}'.");
+            context.Wait(MessageReceived);
+        }
+
+        [QnAMakerResponseHandler(50)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            await context.PostAsync($"I found an answer that might help...{result.Answer}.");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ QnABot/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QnABot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QnABot")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ad865c9-f5cc-4ef3-b519-97bf3c24540b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
